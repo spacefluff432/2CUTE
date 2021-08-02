bc76c919fe0b4e7b879e6134416f678729a9a520
hb432 <hb432@protonmail.com>
2021-08-02 17:32:08 -0400

    OH NO NO NO KEKW

--- demo/assets/script/game/player.js
+++ demo/assets/script/game/player.js
@@ -1,7 +1,8 @@
 import * as assets from './assets.js';
 export const player = new XWalker({
     anchor: { x: 0, y: 1 },
-    size: { x: 20, y: 3 }
+    size: { x: 20, y: 3 },
+    scale: { x: 0.5, y: 0.5 }
 });
 export function swapSprites(key) {
     player.sprites = assets.player[key];
--- src/game/player.ts
+++ src/game/player.ts
@@ -2,7 +2,8 @@ import * as assets from './assets.js';
 
 export const player = new XWalker({
    anchor: { x: 0, y: 1 },
-   size: { x: 20, y: 3 }
+   size: { x: 20, y: 3 },
+   scale: { x: 0.5, y: 0.5 }
 });
 
 export function swapSprites (key: keyof typeof assets.player) {
