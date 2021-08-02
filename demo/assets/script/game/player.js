1b117e9af2ec4fdcd909db5667b2b0ac2f45ad1e
hb432 <hb432@protonmail.com>
2021-08-02 17:33:56 -0400

    that was weird

--- demo/assets/script/game/player.js
+++ demo/assets/script/game/player.js
@@ -2,7 +2,7 @@ import * as assets from './assets.js';
 export const player = new XWalker({
     anchor: { x: 0, y: 1 },
     size: { x: 20, y: 3 },
-    scale: { x: 0.5, y: 0.5 }
+    scale: { x: 1, y: 1 }
 });
 export function swapSprites(key) {
     player.sprites = assets.player[key];
